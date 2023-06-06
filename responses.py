1b8bc5bda7617ad16ebf06b00f9e7ff88e081d46
RyanMConstable <ryanmconstable@gmail.com>
2023-06-06 19:01:38 -0400

    Removed time import and fixed up handle_response

--- responses.py
+++ responses.py
@@ -1,18 +1,21 @@
 import sys
-import time
 sys.path.append('..')
 import CSGO_Project.CSGOsql
 
 ##########################################################
 def handle_response(message, username, usernameID) -> str:
     p_message = message.lower()
+    try:
+        command = p_message.split(" ").lower()
+    except:
+        command = None
 
     #Display all options
     if p_message == "help" or p_message == '-h' or p_message == 'h' or p_message == '-help':
         returnString = "-SteamID <id>" 
         return returnString 
     
-    if p_message.split(" ")[0] == "-steamid":
+    if command == "-steamid":
         CSGO_Project.CSGOsql.setDiscordUser(usernameID, p_message.split(" ")[1])
         return "SteamID updated"
 
