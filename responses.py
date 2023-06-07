2317d07cecfba69ef0fe4ce5a85d8359e51bbe78
RyanMConstable <ryanmconstable@gmail.com>
2023-06-06 21:31:42 -0400

    Added better help message

--- responses.py
+++ responses.py
@@ -10,11 +10,14 @@ def handle_response(message, username, usernameID) -> str:
     except:
         command = None
 
-    #Display all options
+    #Display help messages
     if p_message == "help" or p_message == '-h' or p_message == 'h' or p_message == '-help':
-        returnString = "-SteamID <id>" 
+        returnString = "Commands:\n  -steamid <id>\n  -top10 <category>\n      Categories: [totalkills, score, tk_count, assist, deaths, 5k, 4k, 3k, 2k, 1k, headshot, kd, rws, shot_count, hit_count, flashbang_thrown, smoke_thrown, he_thrown, molly_thrown, incendiary_thrown, decoy_thrown, round_count]" 
         return returnString 
     
+    
+    
+    #This allows a user to add their steamid to the database for future reference
     if command == "-steamid":
         CSGO_Project.CSGOsql.setDiscordUser(usernameID, p_message.split(" ")[1])
         return "SteamID updated"
