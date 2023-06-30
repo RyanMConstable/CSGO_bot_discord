11b742feaeca9ba81beb8a084030b16d4413b73b
RyanMConstable <ryanmconstable@gmail.com>
2023-06-30 14:57:15 -0400

    Added dictionary of commands, this way the bot only responds when you type a command

--- responses.py
+++ responses.py
@@ -4,10 +4,17 @@ import responsesExt
 def handle_response(message, username, usernameID) -> str:
     #This makes sure that any command in any form of caps is met
     p_message = message.lower()
+    
+    #Create dictionary of commands
+    commands = {"-help": 0, "-h": 0, "h": 0, "help":0, "-steamid":0, "-leaders":0, "-top":0, "-givetop":0, "-avg":0, "-bottom":0}
     try:
         command = p_message.split(" ")[0].lower()
     except:
         command = None
+    
+    #Instead of checking every command end it here
+    if command not in commands:
+        return
     
     #Display help message to the user
     if p_message == "help" or p_message == '-h' or p_message == 'h' or p_message == '-help':
@@ -31,11 +38,7 @@ def handle_response(message, username, usernameID) -> str:
     
     #Gives the user bottom of a category
     if command == "-bottom":
-        return responsesExt.bottom(message)
-    
-    #If there was an attempted command but command not found...
-    if p_message[0] == '-':
-        return "Command not found try '-help' or '-h'"
-    
+        return responsesExt.bottom(message) 
+       
     return
 
