748ed59e0096c6652b177611bde438c8dd36f4b4
RyanMConstable <ryanmconstable@gmail.com>
2023-07-29 18:36:25 -0400

    Working totals button

--- bot.py
+++ bot.py
@@ -111,7 +111,8 @@ def run_discord_bot():
             
         @discord.ui.button(label="Totals", style=discord.ButtonStyle.primary)
         async def eighth_button_callback(self, button, interaction):
-            await send_message("")
+            msg = responses.handle_response("-sum", str(self.ctx.author), self.ctx.author.id)
+            await interaction.response.send_message(embed=self.makeEmbed(msg))
             
         @discord.ui.button(label="Summary of Last Game", style=discord.ButtonStyle.primary)
         async def ninth_button_callback(self, button, interaction):
