66c82ce4e46204a93292c5d09bcb1a882cb56892
RyanMConstable <ryanmconstable@gmail.com>
2023-07-29 18:35:15 -0400

    Working position button

--- bot.py
+++ bot.py
@@ -86,7 +86,8 @@ def run_discord_bot():
             
         @discord.ui.button(label="Position", style=discord.ButtonStyle.primary)
         async def third_button_callback(self, button, interaction):
-            await send_message("") 
+            msg = responses.handle_response("-pos", str(self.ctx.author), self.ctx.author.id)
+            await interaction.response.send_message(embed=self.makeEmbed(msg))
             
         @discord.ui.button(label="Last Game", style=discord.ButtonStyle.primary)
         async def fourth_button_callback(self, button, interaction):
