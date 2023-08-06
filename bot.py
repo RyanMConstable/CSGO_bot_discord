72f521b9e85a87500b52e7742511445cbd678f52
RyanMConstable <ryanmconstable@gmail.com>
2023-08-06 17:34:38 -0400

    Changed client to bot

--- bot.py
+++ bot.py
@@ -22,11 +22,11 @@ async def send_message(username, message, user_message, usernameID, is_private =
 
 def run_discord_bot():
     TOKEN = os.environ["DISCORD_TOKEN"]
-    client = discord.Bot(intents=discord.Intents.all())
+    bot = discord.Bot(intents=discord.Intents.all())
     
-    @client.event
+    @bot.event
     async def on_message(message):
-        if message.author == client.user and message.content != "-Summary Time":
+        if message.author == bot.user and message.content != "-Summary Time":
             return
 
         usernameID = str(message.author.id)
@@ -35,24 +35,24 @@ def run_discord_bot():
 
         await send_message(username, message, user_message, usernameID, is_private=False)
         
-    @client.event
+    @bot.event
     async def on_connect():
         print("Connected and ready to go")
         
-    @client.event
+    @bot.event
     async def on_disconnect():
         print("Going dark")
     
-    @client.slash_command(name = "menu", description="Provides a stat gui") # Create a menu command
+    @bot.slash_command(name = "menu", description="Provides a stat gui") # Create a menu command
     async def menu(ctx: discord.ApplicationContext):
         await ctx.respond("", view=view.MyView(ctx))
         
-    @client.slash_command(name = "menu", description="Provides a stat gui") # Create a menu command
+    @bot.slash_command(name = "menu", description="Provides a stat gui") # Create a menu command
     async def omenu(ctx: discord.ApplicationContext, message):
         await ctx.respond("", view=view.MyView(ctx, message))
         
-    @client.slash_command(name = "signup", description="Provides the signup menu") #Sign up view
+    @bot.slash_command(name = "signup", description="Provides the signup menu") #Sign up view
     async def signup(ctx: discord.ApplicationContext):
         await ctx.respond("", view=view.SignUpView())
     
-    client.run(TOKEN)
+    bot.run(TOKEN)
